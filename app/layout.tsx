a725a59cd6b99c2cf6d2f6efd5405e39fa667dcb
Maria Rodrigues <118668589+mulinco@users.noreply.github.com>
2026-02-28 01:33:18 -0300

    feat: adiciona analytics da vercel no projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Ponira Lab | Design & Development",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="pt-br" className="scroll-smooth">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
