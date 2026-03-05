56622fb154749a316572562806e6e4b21bacf93f
Maria Rodrigues <118668589+mulinco@users.noreply.github.com>
2026-03-05 17:06:58 -0300

    fix: teste debug

--- app/page.tsx
+++ app/page.tsx
@@ -13,42 +13,8 @@ import ViewportBlur from "./components/ViewportBlur";
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-ponira-brown overflow-x-hidden">
-      <div className="fixed inset-0 z-0 bg-ponira-brown">
+      <div className="fixed inset-0 z-0">
         <Grainient color1="#5a5539" color2="#3b2e0f" color3="#9b682a" />
-      </div>
-
-      <ViewportBlur />
-
-      <div className="relative z-10">
-        <Navbar />
-
-        <Hero />
-
-        <div id="labs">
-          <SectionReveal>
-            <Labs />
-          </SectionReveal>
-        </div>
-
-        <div id="showcase">
-          <SectionReveal exitAt={0.92}>
-            <Showcase />
-          </SectionReveal>
-        </div>
-
-        <div id="pacotes">
-          <SectionReveal exitAt={0.92}>
-            <CrossLab />
-          </SectionReveal>
-        </div>
-
-        <div id="founders">
-          <SectionReveal exitAt={0.88}>
-            <Founders />
-          </SectionReveal>
-        </div>
-
-        <Footer />
       </div>
     </main>
   );
